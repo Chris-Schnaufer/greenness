6c75177253ed5f80c0f200c05c32d358212603a8
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-04-29 15:52:40 -0700

    Demo

--- algorithm_rgb.py
+++ algorithm_rgb.py
@@ -9,17 +9,17 @@ import numpy as np
 VERSION = '1.0'
 
 # Information on the creator of this algorithm
-ALGORITHM_AUTHOR = 'Unknown'
+ALGORITHM_AUTHOR = 'Chris'
 ALGORITHM_AUTHOR_EMAIL = ''
 ALGORITHM_CONTRIBUTORS = [""]
 
 ALGORITHM_NAME = 'my nifty one'
-ALGORITHM_DESCRIPTION = 'This algorithm calculates the niftyness of RGB plot-level images'
+ALGORITHM_DESCRIPTION = 'This algorithm calculates greenness of RGB plot-level images'
 
 # Citation information for publication (more information in HOW_TO.md)
-CITATION_AUTHOR = 'unknown'
-CITATION_TITLE = ''
-CITATION_YEAR = ''
+CITATION_AUTHOR = 'Me'
+CITATION_TITLE = 'Great paper'
+CITATION_YEAR = '2033'
 
 # The name of one or more variables returned by the algorithm, separated by commas (more information in HOW_TO.md)
 # If only one name is specified, no comma's are used.
@@ -27,16 +27,16 @@ CITATION_YEAR = ''
 # all white space is kept intact; don't add any extra whitespace since it may cause name comparisons
 # to fail.
 # !! Replace the content of this string with your variable names
-VARIABLE_NAMES = 'size of image channels'
+VARIABLE_NAMES = 'Excess greeness'
 
 # Variable units matching the order of VARIABLE_NAMES, also comma-separated.
 # For each variable name in VARIABLE_NAMES add the unit of measurement the value represents.
 # !! Replace the content of this string with your variables' unit
-VARIABLE_UNITS = 'pixels'
+VARIABLE_UNITS = 'percent'
 
 # Variable labels matching the order of VARIABLE_NAMES, also comma-separated.
 # This is an optional definition and can be left empty.
-VARIABLE_LABELS = ''
+VARIABLE_LABELS = 'Excess'
 
 # Optional override for the generation of a BETYdb compatible csv file
 # Set to False to suppress the creation of a compatible file
@@ -56,7 +56,7 @@ def calculate(pxarray: np.ndarray):
         Returns one or more calculated values
     """
     # ALGORITHM: replace the following lines with your algorithm
-    channel_size = pxarray[:, :, 1].size
+    excess = np.sum(2 * pxarray[:, :, 1] - (pxarray[:, :, 0] + pxarray[:, :, 2]))
 
     # RETURN: replace the following return with your calculated values. Be sure to order them as defined in VARIABLE_NAMES above
-    return channel_size
+    return excess / 10000.0
